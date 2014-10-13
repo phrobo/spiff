cdba0c629afeaaf213758ea0794140a90412b2e8
Torrie Fischer <tdfischer@hackerbots.net>
2014-10-13 00:50:51 -0700

    Fix up CSS and javascript overload

--- client/html/Gruntfile.js
+++ client/html/Gruntfile.js
@@ -3,30 +3,52 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        mangle: false
+        mangle: false,
+        beautify: true,
+        sourceMap: true
       },
       deps: {
-        files: [{
-          expand: true,
-          cwd: 'app/lib',
-          src: '**/*.js',
-          dest: 'app/lib'
-        }]
+        files: {
+          'app/lib/deps.js': [
+            'bower_components/jquery/jquery.js',
+            'bower_components/bootstrap/docs/assets/js/bootstrap.js',
+            'bower_components/jquery-ui/ui/jquery-ui.js',
+            'bower_components/jquery-qrcode/jquery.qrcode.min.js',
+            'bower_components/underscore/underscore.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/restangular/dist/restangular.js',
+            'bower_components/angular-ui-bootstrap-bower/ui-bootstrap.js',
+            'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
+            'bower_components/angular-ui-router/release/angular-ui-router.js',
+            'bower_components/angular-gravatar/build/md5.js',
+            'bower_components/angular-gravatar/build/angular-gravatar.js',
+            'bower_components/showdown/src/showdown.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/angular-markdown-directive/markdown.js'
+          ]
+        }
       },
       app: {
-        files: [{
-          expand: true,
-          cwd: 'app/js',
-          src: '**/*.js',
-          dest: 'app/lib',
-        }]
+        files: {
+          'app/lib/app.js': [
+            'app/js/**/*.js'
+          ]
+        }
       }
     },
-    bower: {
-      install: {
-        options: {
-          targetDir: "app/lib",
+    less: {
+      bootstrap: {
+        files: {
+          'app/lib/bootstrap.css': 'bower_components/bootstrap/less/bootstrap.less'
         }
+      }
+    },
+    sass: {
+      app: {
+        files: {
+          'app/lib/app.css': 'app/lib/app.scss',
+        },
       }
     },
     watch: {
@@ -45,9 +67,10 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['bower', 'uglify:deps', 'uglify:app']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.registerTask('default', ['less', 'sass', 'uglify:deps', 'uglify:app']);
 
 };
